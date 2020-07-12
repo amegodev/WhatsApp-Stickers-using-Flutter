4c42cebb1e22b66c79805e9da97fbb1b2cb94bfa
amegos <amegos11@gmail.com>
2020-07-12 13:04:02 +0100

    Update home_page_tow.dart

--- lib/screens/home_page_tow.dart
+++ lib/screens/home_page_tow.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePageTow extends StatefulWidget {
   @override
@@ -10,6 +11,7 @@ class _HomePageTowState extends State<HomePageTow> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: SvgPicture.asset('assets/icons/burger_menu.svg'),
         title: Text('500+ Arabic Stickers', style: TextStyle(
           fontWeight: FontWeight.w700,
         ),),
